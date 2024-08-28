b207a1bf5e7350d3e3db56c33e1353611b848534
Joe Schmetzer <joe.schmetzer@penten.com>
2024-08-28 14:56:30 +1000

    Rename Coords -> HitBox

--- src/pfk_games/stickman/hitbox.py
+++ src/pfk_games/stickman/hitbox.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+
 class Point:
     def __init__(self, x: int = 0, y: int = 0) -> None:
         self._x = x
@@ -16,10 +17,10 @@ class Point:
     def move(self, dx: int, dy: int) -> Point:
         self._x += dx
         self._y += dy
-        return self
+        return Point(self._x + dx, self._y + dy)
 
 
-class Coords:
+class HitBox:
     def __init__(self, top_left: Point = Point(), bottom_right: Point = Point) -> None:
         self._top_left = top_left
         self._bottom_right = bottom_right
@@ -48,30 +49,29 @@ class Coords:
     def bottom(self) -> int:
         return self._bottom_right.y
 
-    def within_x(self, other: Coords) -> bool:
+    def within_x(self, other: HitBox) -> bool:
         return (other.left < self.left < other.right) or \
                (other.left < self.right < other.right) or \
                (self.left < other.left < self.right) or \
                (self.left < other.right < self.right)
 
-    def within_y(self, other: Coords) -> bool:
+    def within_y(self, other: HitBox) -> bool:
         return (other.top < self.top < other.bottom) or \
                (other.top < self.bottom < other.bottom) or \
                (self.top < other.top < self.bottom) or \
                (self.top < other.bottom < self.bottom)
 
-    def collided_left(self, other: Coords) -> bool:
+    def collided_left(self, other: HitBox) -> bool:
         return self.within_y(other) and other.left <= self.left <= other.right
 
-    def collided_right(self, other: Coords) -> bool:
+    def collided_right(self, other: HitBox) -> bool:
         return self.within_y(other) and other.left <= self.right <= other.right
 
-    def collided_top(self, other: Coords) -> bool:
+    def collided_top(self, other: HitBox) -> bool:
         return self.within_x(other) and other.top <= self.top <= other.bottom
 
-    def collided_bottom(self, other: Coords, y: int) -> bool:
+    def collided_bottom(self, other: HitBox, y: int) -> bool:
         return self.within_x(other) and other.top <= (self.bottom + y) <= other.bottom
 
-    def move(self, dx, dy):
-        self._top_left.move(dx, dy)
-        self._bottom_right.move(dx, dy)
+    def move(self, dx, dy) -> HitBox:
+        return HitBox(self.top_left.move(dx, dy), self.bottom_right.move(dx, dy))
--- src/pfk_games/stickman/sprite.py
+++ src/pfk_games/stickman/sprite.py
@@ -2,28 +2,36 @@ from __future__ import annotations
 import time
 import tkinter as tk
 
-from pfk_games.stickman.coords import Coords, Point
+from pfk_games.stickman.hitbox import HitBox, Point
 from pfk_games.stickman.images import image_path
 
 
 class Sprite:
-    def __init__(self, canvas: tk.Canvas) -> None:
+    def __init__(self, canvas: tk.Canvas, hitbox: HitBox = HitBox()) -> None:
         self._canvas = canvas
         self._canvas_width: int = canvas.winfo_width()
         self._canvas_height: int = canvas.winfo_height()
+        self._hitbox = hitbox
         self._endgame = False
-        self._coordinates = Coords()
 
     def tick(self) -> None:
         pass
 
     @property
-    def coords(self) -> Coords:
-        return self._coordinates
+    def hitbox(self) -> HitBox:
+        return self._hitbox
 
     @property
     def endgame(self) -> bool:
         return self._endgame
+
+
+def _hitbox_from(image: tk.PhotoImage, x: int, y: int, width: int, height: int) -> HitBox:
+    if width == 0:
+        width = image.width()
+    if height == 0:
+        height = image.height()
+    return HitBox(Point(x, y), Point(x + width, y + height))
 
 
 class PlatformSprite(Sprite):
@@ -32,20 +40,20 @@ class PlatformSprite(Sprite):
                  source_image: tk.PhotoImage,
                  x: int, y: int,
                  width: int = 0, height: int = 0) -> None:
-        super().__init__(canvas)
-
-        if width == 0:
-            width = source_image.width()
-        if height == 0:
-            height = source_image.height()
-
+        super().__init__(canvas, _hitbox_from(source_image, x, y, width, height))
         self._source_image = source_image
         self._canvas_image = canvas.create_image(x, y, image=source_image, anchor="nw")
-        self._coordinates = Coords(Point(x, y), Point(x + width, y + height))
+
+
+def _initial_stickman_hitbox() -> HitBox:
+    top_left = Point(200, 470)
+    bottom_right = Point(top_left.x + 27, top_left.y + 30)
+    return HitBox(top_left, bottom_right)
+
 
 class StickFigureSprite(Sprite):
     def __init__(self, canvas: tk.Canvas, sprites: list[Sprite]) -> None:
-        super().__init__(canvas)
+        super().__init__(canvas, _initial_stickman_hitbox())
         self._sprites = sprites
         self._images_left = [
             tk.PhotoImage(file=image_path("figure-l1.png")),
@@ -57,10 +65,7 @@ class StickFigureSprite(Sprite):
             tk.PhotoImage(file=image_path("figure-r2.png")),
             tk.PhotoImage(file=image_path("figure-r3.png"))
         ]
-        top_left = Point(200, 470)
-        bottom_right = Point(top_left.x + 27, top_left.y + 30)
-        self._coordinates = Coords(top_left, bottom_right)
-        self._canvas_image = canvas.create_image(top_left.x, top_left.y, image=self._images_left[0], anchor="nw")
+        self._canvas_image = canvas.create_image(self.hitbox.top_left.x, self.hitbox.top_left.y, image=self._images_left[0], anchor="nw")
         self._dx = 0
         self._dy = 0
         self._current_index = 0
@@ -119,8 +124,8 @@ class StickFigureSprite(Sprite):
         for sprite in self._sprites:
             on_platform = self._check_collision(sprite, on_platform)
 
-        self._canvas.move(self._canvas_image, self._dx, self._dy)
-        self._coordinates.move(self._dx, self._dy)
+        self._hitbox = self._hitbox.move(self._dx, self._dy)
+        self._canvas.moveto(self._canvas_image, self._hitbox.top_left.x, self._hitbox.top_left.y)
 
     def _moving_left(self) -> bool:
         return self._dx < 0
@@ -155,19 +160,19 @@ class StickFigureSprite(Sprite):
         self._check_bottom_edge()
 
     def _check_bottom_edge(self) -> None:
-        if self._moving_down() and self.coords.bottom >= self._canvas_height:
+        if self._moving_down() and self.hitbox.bottom >= self._canvas_height:
             self._stop_vertical()
 
     def _check_top_edge(self) -> None:
-        if self._moving_up() and self.coords.top <= 0:
+        if self._moving_up() and self.hitbox.top <= 0:
             self._stop_vertical()
 
     def _check_left_edge(self) -> None:
-        if self._moving_left() and self.coords.left <= 0:
+        if self._moving_left() and self.hitbox.left <= 0:
             self._stop_horizontal()
 
     def _check_right_edge(self) -> None:
-        if self._moving_right() and self.coords.right >= self._canvas_width:
+        if self._moving_right() and self.hitbox.right >= self._canvas_width:
             self._stop_horizontal()
 
     def _check_collision(self, sprite: Sprite, on_platform: bool) -> bool:
@@ -176,10 +181,10 @@ class StickFigureSprite(Sprite):
         if self._has_hit_top(sprite):
             self._dy = -self._dy
         if self._will_hit_bottom(sprite):
-            self._dy = max(0, sprite.coords.top - self.coords.bottom)
+            self._dy = max(0, sprite.hitbox.top - self.hitbox.bottom)
         if (not on_platform and self._dy == 0 and
-                self.coords.bottom < self._canvas_height and
-                self.coords.collided_bottom(sprite.coords, 1)):
+                self.hitbox.bottom < self._canvas_height and
+                self.hitbox.collided_bottom(sprite.hitbox, 1)):
             on_platform = True
         if self._has_hit_left(sprite):
             self._stop_horizontal()
@@ -189,40 +194,33 @@ class StickFigureSprite(Sprite):
             self._stop_horizontal()
             if sprite.endgame:
                 self._endgame = True
-        if not on_platform and self._dy == 0 and self.coords.bottom < self._canvas_height:
+        if not on_platform and self._dy == 0 and self.hitbox.bottom < self._canvas_height:
             self._dy = 4
         return on_platform
 
     def _has_hit_right(self, sprite: Sprite) -> bool:
-        return self._moving_right() and self.coords.collided_right(sprite.coords)
+        return self._moving_right() and self.hitbox.collided_right(sprite.hitbox)
 
     def _has_hit_left(self, sprite: Sprite) -> bool:
-        return self._moving_left() and self.coords.collided_left(sprite.coords)
+        return self._moving_left() and self.hitbox.collided_left(sprite.hitbox)
 
     def _has_hit_top(self, sprite: Sprite) -> bool:
-        return self._moving_up() and self.coords.collided_top(sprite.coords)
+        return self._moving_up() and self.hitbox.collided_top(sprite.hitbox)
 
     def _will_hit_bottom(self, sprite: Sprite) -> bool:
-        return self._moving_down() and self.coords.collided_bottom(sprite.coords, self._dy)
+        return self._moving_down() and self.hitbox.collided_bottom(sprite.hitbox, self._dy)
+
+
+def _door_hitbox(x: int, y: int, width: int, height: int) -> HitBox:
+    return HitBox(Point(x, y), Point(x + int(width / 2), y + int(height / 2)))
 
 
 class DoorSprite(Sprite):
-    def __init__(self,
-                 canvas: tk.Canvas,
-                 x: int, y: int,
-                 width: int = 0, height: int = 0):
-        super().__init__(canvas)
-
+    def __init__(self, canvas: tk.Canvas, x: int, y: int, width: int, height: int):
+        super().__init__(canvas, _door_hitbox(x, y, width, height))
         self._closed_door = tk.PhotoImage(file=image_path("door1.png"))
         self._open_door = tk.PhotoImage(file=image_path("door2.png"))
-
-        if width == 0:
-            width = self._closed_door.width()
-        if height == 0:
-            height = self._closed_door.height()
-
         self._canvas_image = canvas.create_image(x, y, image=self._closed_door, anchor="nw")
-        self._coordinates = Coords(Point(x, y), Point(x + int(width / 2), y + int(height / 2)))
         self._is_open = False
         self._endgame = True
 
